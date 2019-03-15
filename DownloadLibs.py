05535202482e0b43e792de1e026676a5429e48b2
Matias <matias@dimension10.com>
2019-03-15 10:46:16 +0100

    Replaced ExternalLibraries download link with OneDrive (since direct downloading of large files doesn't work that nice on Google Driv e)

--- DownloadLibs.py
+++ DownloadLibs.py
@@ -2,6 +2,6 @@ from StringIO import StringIO
 from zipfile import ZipFile
 from urllib import urlopen
 
-resp = urlopen("https://docs.google.com/uc?export=download&id=1p4GA1lcoKZZcPsGSPOZ6ZWb85yyvmm2b")
+resp = urlopen("https://onedrive.live.com/download?cid=C1835F35C0EF9E9D&resid=C1835F35C0EF9E9D%2114549&authkey=AAik1ummNmp-wb4")
 zipfile = ZipFile(StringIO(resp.read()))
 zipfile.extractall("")
