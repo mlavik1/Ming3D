47ea663ef1c7756f0350182f62621a9f09f0cee3
Matias <matias@dimension10.com>
2019-04-03 11:05:25 +0200

    Fixed NativeUI-project not building on Win64

--- NativeUI/Source/button.cpp
+++ NativeUI/Source/button.cpp
@@ -1,5 +1,6 @@
 ﻿#include "button.h"
 #include "text_utils.h"
+#include <WinUser.h>
 
 namespace NativeUI
 {
@@ -10,7 +11,11 @@ namespace NativeUI
 	{
         mHwnd = CreateWindow(L"BUTTON", L"", WS_CHILD | WS_VISIBLE /*| BS_BITMAP*/, 0, 0, 50, 50, arg_parent->GetHwnd(), NULL, GetModuleHandle(NULL), NULL);
 		//mHwnd = CreateWindow(L"NativeUIButton", L"", WS_CHILD | WS_VISIBLE, 0, 0, 50, 50, arg_parent->GetHwnd(), NULL, GetModuleHandle(NULL), NULL);
+#ifdef _WIN64
+        mOrigWndProc = (WNDPROC)SetWindowLongPtr(mHwnd, GWLP_WNDPROC, (LONG)WndProc);
+#else
         mOrigWndProc = (WNDPROC)SetWindowLong(mHwnd, GWL_WNDPROC, (LONG)WndProc);
+#endif
         //mWinButtonHWND = CreateWindow(L"BUTTON", L"", WS_CHILD | WS_VISIBLE /*| BS_BITMAP*/, 0, 0, 50, 50, mHwnd, (HMENU)mWinButtonUID, GetModuleHandle(NULL), NULL);
         SetSize(Point(80.0f, 40.0f));
 		ShowWindow(mHwnd, TRUE);
